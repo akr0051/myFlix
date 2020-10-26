079cd888a15fc6c1db96d85126f7db51b4a5753b
akr0051 <70978103+akr0051@users.noreply.github.com>
2020-10-26 17:34:53 -0500

    Create models.js
    created schemas for mongoose

---
+++ models.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+let movieSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String,
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean,
+});
+
+let userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
+});
+
+let directorSchema = mongoose.Schema({
+  Name: String,
+  Bio: String,
+  Birth: Date,
+  Death: Date,
+});
+
+let genreSchema = mongoose.Schema({
+  Name: String,
+  Description: String,
+});
+
+let Movie = mongoose.model("Movie", movieSchema);
+let User = mongoose.model("User", userSchema);
+let Genre = mongoose.model("Genre", genreSchema);
+let Director = mongoose.model("Director", directorSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
+module.exports.Director = Director;
+module.exports.Genre = Genre;
