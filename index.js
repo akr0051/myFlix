ad9e548aafd7da7cf61052c28ef3a7f405add301
akr0051 <70978103+akr0051@users.noreply.github.com>
2020-11-09 12:40:36 -0600

    updated cors policy

--- index.js
+++ index.js
@@ -30,6 +30,15 @@ require("./passport");
 
 app.use(morgan("common"));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/documentation", (req, res) => {
   res.sendFile("public/documentation.html", { root: __dirname });
 });
