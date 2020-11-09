b88dcfdddeafc4535bbab0dc582d1478f857b706
akr0051 <70978103+akr0051@users.noreply.github.com>
2020-11-09 14:03:38 -0600

    changed cors policy

--- index.js
+++ index.js
@@ -24,20 +24,12 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.options("*", cors());
 let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
 
 app.use(morgan("common"));
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.get("/documentation", (req, res) => {
   res.sendFile("public/documentation.html", { root: __dirname });
